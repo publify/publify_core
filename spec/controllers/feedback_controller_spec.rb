fdb1ca3fc43f6cb60d8d4d9462889a23a8b62f44
Matijs van Zuijlen <matijs@matijs.net>
2018-10-15 13:24:32 +0200

    Move all view-rendering controller specs to main project

--- spec/controllers/feedback_controller_spec.rb
+++ spec/controllers/feedback_controller_spec.rb
@@ -34,20 +34,6 @@ RSpec.describe FeedbackController, type: :controller do
       it 'renders the index template' do
         expect(response).to render_template('feedback/index')
       end
-
-      context 'with rendered views' do
-        render_views
-
-        it 'renders a valid atom feed with 4 items' do
-          assert_atom10 response.body, 4
-        end
-
-        it 'renders each item with the correct template' do
-          expect(response).
-            to render_template(partial: 'shared/_atom_item_comment', count: 2).
-            and render_template(partial: 'shared/_atom_item_trackback', count: 2)
-        end
-      end
     end
 
     context 'with rss format' do
@@ -63,20 +49,6 @@ RSpec.describe FeedbackController, type: :controller do
 
       it 'renders the index template' do
         expect(response).to render_template('feedback/index')
-      end
-
-      context 'with rendered views' do
-        render_views
-
-        it 'renders a valid rss feed with 4 items' do
-          assert_rss20 response.body, 4
-        end
-
-        it 'renders each item with the correct template' do
-          expect(response).
-            to render_template(partial: 'shared/_rss_item_comment', count: 2).
-            and render_template(partial: 'shared/_rss_item_trackback', count: 2)
-        end
       end
     end
   end
