59afe5cc7ce15cb60b74b47a675eed2655f01acb
Matijs van Zuijlen <matijs@matijs.net>
2023-09-29 13:47:44 +0200

    Autocorrect Capybara/RSpec/HaveSelector offenses

--- spec/helpers/author_helper_spec.rb
+++ spec/helpers/author_helper_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe AuthorsHelper, type: :helper do
   describe "display_profile_item" do
     it "displays the item as a list item if show_item is true" do
       item = display_profile_item("my@jabber.org", "Jabber:")
-      expect(item).to have_selector("li", text: "Jabber: my@jabber.org")
+      expect(item).to have_css("li", text: "Jabber: my@jabber.org")
     end
 
     it "does not display the item empty" do
@@ -16,7 +16,7 @@ RSpec.describe AuthorsHelper, type: :helper do
 
     it "displays a link if the item is an url" do
       item = display_profile_item("http://twitter.com/mytwitter", "Twitter:")
-      expect(item).to have_selector("li") do
+      expect(item).to have_css("li") do
         have_link("http://twitter.com/mytwitter")
       end
     end
