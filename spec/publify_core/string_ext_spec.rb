be29354d2325e975fb98a3e9ce48a08ecf8c4ddd
Matijs van Zuijlen <matijs@matijs.net>
2026-06-12 17:29:40 +0200

    Move and improve specs for StringExt#to_url

--- spec/publify_core/string_ext_spec.rb
+++ spec/publify_core/string_ext_spec.rb
@@ -11,8 +11,45 @@ RSpec.describe PublifyCore::StringExt do
   end
 
   describe "to_url" do
-    it "gives a proper space-less, trimmed URL" do
-      expect(" this is  a sentence ".to_url).to eq("this-is-a-sentence")
+    it "downcases the string" do
+      expect("Article-3".to_url).to eq "article-3"
+    end
+
+    it "strips trailing special characters" do
+      expect("Article 3!?#".to_url).to eq "article-3"
+    end
+
+    it "replaces special characters with a dash" do
+      expect("foo@bar".to_url).to eq "foo-bar"
+    end
+
+    it "handles more than two words" do
+      expect("There is Sex in my Violence!".to_url).to eq "there-is-sex-in-my-violence"
+    end
+
+    it "strips leading and trailing dashes" do
+      expect("-article-".to_url).to eq "article"
+    end
+
+    it "handles punctuation in the middle of th string" do
+      expect("Lorem ipsum dolor sit amet, consectetaur adipisicing elit".to_url)
+        .to eq "lorem-ipsum-dolor-sit-amet-consectetaur-adipisicing-elit"
+    end
+
+    it "drops leading and trailing spaces" do
+      expect(" this is a sentence ".to_url).to eq("this-is-a-sentence")
+    end
+
+    it "compresses multiple spaces to a single space" do
+      expect("this is  a sentence".to_url).to eq("this-is-a-sentence")
+    end
+
+    it "drops single quotes instead of replacing them with a dash" do
+      expect("My Cat's Best Friend".to_url).to eq "my-cats-best-friend"
+    end
+
+    it "drops double quotes instead of replacing them with a dash" do
+      expect("bar\"baz".to_url).to eq "barbaz"
     end
   end
 end
