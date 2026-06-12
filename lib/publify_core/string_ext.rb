5ef072eff85183cfef6a7804df6eb0695218e010
Matijs van Zuijlen <matijs@matijs.net>
2026-06-12 13:44:52 +0200

    Remove StringExt#strip_html and supporting constants

--- lib/publify_core/string_ext.rb
+++ lib/publify_core/string_ext.rb
@@ -34,17 +34,6 @@ module PublifyCore
     def to_title(item, settings, params)
       TitleBuilder.new(self).build(item, settings, params)
     end
-
-    # Strips any html markup from a string
-    TYPO_TAG_KEY = TYPO_ATTRIBUTE_KEY = /[\w:_-]+/
-    TYPO_ATTRIBUTE_VALUE = /(?:[A-Za-z0-9]+|(?:'[^']*?'|"[^"]*?"))/
-    TYPO_ATTRIBUTE = /(?:#{TYPO_ATTRIBUTE_KEY}(?:\s*=\s*#{TYPO_ATTRIBUTE_VALUE})?)/
-    TYPO_ATTRIBUTES = /(?:#{TYPO_ATTRIBUTE}(?:\s+#{TYPO_ATTRIBUTE})*)/
-    TAG = %r{<[!/?\[]?(?:#{TYPO_TAG_KEY}|--)(?:\s+#{TYPO_ATTRIBUTES})?\s*(?:[!/?\]]+|--)?>}
-
-    def strip_html
-      gsub(TAG, "").gsub(/\s+/, " ").strip
-    end
   end
 end
 
