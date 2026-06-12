8005f967e652185aa0f43606677e5fbc9accb542
Matijs van Zuijlen <matijs@matijs.net>
2026-06-12 18:41:29 +0200

    Make StringExt#to_url an implementation detail of #to_permalink
    With this change, #to_url can no longer be used.

--- spec/publify_core/string_ext_spec.rb
+++ spec/publify_core/string_ext_spec.rb
@@ -13,48 +13,47 @@ RSpec.describe PublifyCore::StringExt do
       expect("This <i>is</i> a <b>test</b>".to_permalink)
         .to eq "this-is-a-test"
     end
-  end
 
-  describe "to_url" do
     it "downcases the string" do
-      expect("Article-3".to_url).to eq "article-3"
+      expect("Article-3".to_permalink).to eq "article-3"
     end
 
     it "strips trailing special characters" do
-      expect("Article 3!?#".to_url).to eq "article-3"
+      expect("Article 3!?#".to_permalink).to eq "article-3"
     end
 
     it "replaces special characters with a dash" do
-      expect("foo@bar".to_url).to eq "foo-bar"
+      expect("foo@bar".to_permalink).to eq "foo-bar"
     end
 
     it "handles more than two words" do
-      expect("There is Sex in my Violence!".to_url).to eq "there-is-sex-in-my-violence"
+      expect("There is Sex in my Violence!".to_permalink)
+        .to eq "there-is-sex-in-my-violence"
     end
 
     it "strips leading and trailing dashes" do
-      expect("-article-".to_url).to eq "article"
+      expect("-article-".to_permalink).to eq "article"
     end
 
     it "handles punctuation in the middle of th string" do
-      expect("Lorem ipsum dolor sit amet, consectetaur adipisicing elit".to_url)
+      expect("Lorem ipsum dolor sit amet, consectetaur adipisicing elit".to_permalink)
         .to eq "lorem-ipsum-dolor-sit-amet-consectetaur-adipisicing-elit"
     end
 
     it "drops leading and trailing spaces" do
-      expect(" this is a sentence ".to_url).to eq("this-is-a-sentence")
+      expect(" this is a sentence ".to_permalink).to eq("this-is-a-sentence")
     end
 
     it "compresses multiple spaces to a single space" do
-      expect("this is  a sentence".to_url).to eq("this-is-a-sentence")
+      expect("this is  a sentence".to_permalink).to eq("this-is-a-sentence")
     end
 
-    it "drops single quotes instead of replacing them with a dash" do
-      expect("My Cat's Best Friend".to_url).to eq "my-cats-best-friend"
+    it "replaces single quotes with a dash" do
+      expect("My Cat's Best Friend".to_permalink).to eq "my-cat-s-best-friend"
     end
 
     it "drops double quotes instead of replacing them with a dash" do
-      expect("bar\"baz".to_url).to eq "barbaz"
+      expect("bar\"baz".to_permalink).to eq "barbaz"
     end
   end
 end
